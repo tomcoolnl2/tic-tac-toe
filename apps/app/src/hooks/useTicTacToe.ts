cf64c1099d7431424bdf1d5f314ecb0c2b766554
Tom Cool <tom.cool@tomtom.com>
2021-12-22 15:09:43 +0100

    converted to bitwise logic

--- apps/app/src/hooks/useTicTacToe.ts
+++ apps/app/src/hooks/useTicTacToe.ts
@@ -19,8 +19,8 @@ export interface TicTacToe {
     handleRestart: () => void
 }
 
-function createBoard<T>(fill: T): T[] {
-    return Array(9).fill(fill)
+function createBoard<T>(fill: T, size = 9): T[] {
+    return Array(size).fill(fill)
 }
 
 function insertMove<T>(board: T[], index: number, value: T): T[] {
@@ -29,18 +29,37 @@ function insertMove<T>(board: T[], index: number, value: T): T[] {
     return clone
 }
 
-export function useTicTacToe(): TicTacToe {
+const toHexString = (n: number): string => `0x${n.toString(16)}`
 
-    const [ bitBoards, setBitBoards] = useState<[number, number]>([0x0, 0x0])
+const toBinString = (n: number): string => `0b${(n >>> 0).toString(2)}`
+
+const log = ([ boardX, boardO ]: [number, number]): void => {
+    console.table({
+        dec: [boardX, boardO],
+        hex: [toHexString(boardX), toHexString(boardO)],
+        bin: [toBinString(boardX), toBinString(boardO)]
+    })
+}
+
+export function useTicTacToe(): TicTacToe {
+    // a bitboard containing two bits, representing 'X' and 'O'
+    const [ bitBoard, setBitBoard] = useState<[number, number]>([0x0, 0x0])
+    // the index of the cell that was selected
+    const [ cellIndex, setCellIndex ] = useState<number>(-1)
+    // a 3x3 grid, representing the UI of the game
     const [ playersBoard, setPlayersBoard ] = useState<string[]>(createBoard(''))
+    // who's turn is it
     const [ currentPlayer, setCurrentPlayer ] = useState<Player>(0)
+    // do we have a winner?
     const [ winnerName, setWinnerName ] = useState<string | null>(null)
+    // keeps track of the different game status screens
     const [ gameStatus, setGameStatus ] = useState<GameState>(GameState.CREATED)
+    // contestants name
     const [ playerNames, setPlayerNames ] = useState<string[]>(['', ''])
 
     useEffect(() => {
 
-        if (gameStatus !== GameState.STARTED) {
+        if (cellIndex < 0 || gameStatus !== GameState.STARTED) {
             return
         }
 
@@ -50,62 +69,66 @@ export function useTicTacToe(): TicTacToe {
          *  0 0 0   0 0 0   1 1 1   1 0 0   0 1 0   0 0 1   0 0 1   1 0 0
          *  0x1C0   0x038   0x007   0x124   0x092   0x049   0x111   0x054
          */
-        const hexMasks = [0x1C0, 0x038, 0x007, 0x124, 0x092, 0x049, 0x111, 0x054]
-        // const decMasks = [448, 56, 7, 292, 146, 73, 273, 84]
+         const solutions: number[] = [0x1C0, 0x038, 0x007, 0x124, 0x092, 0x049, 0x111, 0x054]
+        // const solutions = [448, 56, 7, 292, 146, 73, 273, 84]
+
+        const bitMask = 0x100 // 256
         
-        const winningPositions = [
-            // horizontals
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            // verticals
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            // diagonals
-            [0, 4, 8],
-            [2, 4, 6]
-        ]
-
-        let winningPositionsIndex = 0
-        let winningPlayer: Player | -1 = -1
-        const n = winningPositions.length
-
-        while (winningPositionsIndex < n && winningPlayer < 0) {
+        /**
+         *   0   1  2  3  4  5  6  7  8
+         *        >> 0x100 = 
+         * 256 128 64 32 16  8  4  2  1
+         */
+        const mask = bitMask >> cellIndex
         
-            const boardValuesToCkeck = winningPositions[winningPositionsIndex].map(index => playersBoard[index])
+        // X and O are represented by a single (hex) number and when updated, tested against all solution masks
+        let [ bitBoardX, bitBoardO ] = bitBoard
 
-            const checkingValue = boardValuesToCkeck[0] as 'X' | 'O' | ''
-            const isFinished = boardValuesToCkeck.every(value => checkingValue && value === checkingValue)
+        // prevent double selecting of cells by checking if neither bitboard has changed
+        if (!((bitBoardX | bitBoardO) & mask)) {
+        
+            // update UI board
+            const cellValue = 'XO'[currentPlayer as number]
+            const updatedBoard = insertMove(playersBoard, cellIndex, cellValue)
+            setPlayersBoard(updatedBoard)
+        
+            // add bits to current players' bitboard
+            bitBoard[currentPlayer] |= mask;
+            ([ bitBoardX, bitBoardO ] = bitBoard) // re-destructure
+        
+            const weHaveAWinner = solutions.some(mask => (bitBoard[currentPlayer] & mask) === mask)
             
-            if (isFinished) {
-                winningPlayer = checkingValue === 'X' ? 0 : 1
-                setWinnerName(playerNames[winningPlayer])
+            if (weHaveAWinner) {
+                setWinnerName(playerNames[currentPlayer])
                 setGameStatus(GameState.FINISHED)
-                return
             }
-
-            winningPositionsIndex += 1
+            else if ((bitBoardX | bitBoardO) === 0x1ff /*511*/) {
+                console.log('This is a draw!')
+            }
+            else {
+                // switch turns
+                setCurrentPlayer((currentPlayer ^ 1) as Player)
+            }
         }
 
-        setGameStatus(playersBoard.filter(value => !value).length ? GameState.STARTED : GameState.FINISHED)
+        log(bitBoard)
 
-    }, [playersBoard, gameStatus])
+    }, [cellIndex, gameStatus])
 
     const handleClick = (cellIndex: number): void => {
-        const cellValue = 'XO'[currentPlayer as number]
-        const updatedBoard = insertMove(playersBoard, cellIndex, cellValue)
-        setPlayersBoard(updatedBoard)
-        setCurrentPlayer((currentPlayer ^ 1) as Player)
+        setCellIndex(cellIndex)
     }
 
     const handleStart = (players: string[], startingPlayer: Player = 0): void => {
         setPlayerNames(players)
+        setPlayersBoard(createBoard(''))
         setCurrentPlayer(startingPlayer)
         setGameStatus(GameState.STARTED)
     }
 
     const handleRestart = () => {
+        setBitBoard([0x0, 0x0])
+        setCellIndex(-1)
         setPlayersBoard(createBoard(''))
         setWinnerName(null)
         setGameStatus(GameState.CREATED)
@@ -123,78 +146,10 @@ export function useTicTacToe(): TicTacToe {
 }
 
 
-
-// /**
-//  *  1 1 1   0 0 0   0 0 0   1 0 0   0 1 0   0 0 1   1 0 0   0 0 1
-//  *  0 0 0   1 1 1   0 0 0   1 0 0   0 1 0   0 0 1   0 1 0   0 1 0
-//  *  0 0 0   0 0 0   1 1 1   1 0 0   0 1 0   0 0 1   0 0 1   1 0 0
-//  *  0x1C0   0x038   0x007   0x124   0x092   0x049   0x111   0x054
-//  */
-//  const solutions: number[] = [0x1C0, 0x038, 0x007, 0x124, 0x092, 0x049, 0x111, 0x054]
-
-//  let board: [number, number] = [0x0, 0x0]
-//  let player: 0 | 1 = 0
- 
-//  const cells: HTMLButtonElement[] = Array.from(document.querySelectorAll('.cell'))
-//  cells.forEach(cell => cell.addEventListener('click', () => move(cell)))
- 
-//  const newGame = (): void => {
-//    board = [0x0, 0x0]
-//    player = 0
-//    cells.forEach(cell => cell.innerHTML = '')
-//  }
- 
-//  const toHexString = (n: number): string => `0x${n.toString(16)}`
-//  const toBinString = (n: number): string => `0b${(n >>> 0).toString(2)}`
- 
-//  const log = ([ boardX, boardO ]: [number, number]): void => {
-//    console.table({
-//      dec: [boardX, boardO],
-//      hex: [toHexString(boardX), toHexString(boardO)],
-//      bin: [toBinString(boardX), toBinString(boardO)]
-//    })
-//  }
  
 //  const move = (cell: HTMLButtonElement): void => {
    
-//    const decToBitMask = 0x100 // 256
-   
-//    /**
-//     *  0-8 decimal to a bitwise 256-1 grid representation
-//     *
-//     *  0   1   2    >>    256 128  64   
-//     *  3   4   5    >>     32  16   8   
-//     *  6   7   8    >>      4   2   1   
-//     */
-//    const mask = decToBitMask >> parseInt(cell.value, 10)
-   
-//    // X and O are represented by a single number and tested by every 
-//    // mask that tests the binary representation of that number to the hex masks (solutions) provided
-//    let [boardX, boardO] = board
-   
-//    // prevent double selecting of cells by checking if either board will change
-//    if (!((boardX | boardO) & mask)) {
-     
-//      const playerSign: 'X' | 'O' = 'XO'[player]
-//      cell.innerHTML = playerSign
-     
-//      board[player] |= mask; // add bits to current players' bitboard
-//      ([boardX, boardO] = board) // re-destructure
- 
-//      const weHaveAWinner = solutions.some(mask => (board[player] & mask) === mask)
-     
-//      if (weHaveAWinner) {
-//        alert(playerSign + ' has won!')
-//        newGame()
-//      }
-//      else if((boardX | boardO) === 0x1ff /*511*/) {
-//        alert('This is a draw!')
-//        newGame()
-//      }
-//      else {
-//        player ^= 1 // switch turns
-//      }
-//    }
+
    
 //    log(board)
 //  }
