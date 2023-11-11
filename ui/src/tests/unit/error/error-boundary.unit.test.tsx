d6fa9710b2d4092eb63eb11f45504bd382a777eb
coolt <tom.cool@tomtom.com>
2023-11-11 13:10:51 +0800

    test: mock console.error

--- ui/src/tests/unit/error/error-boundary.unit.test.tsx
+++ ui/src/tests/unit/error/error-boundary.unit.test.tsx
@@ -4,13 +4,27 @@ import { ErrorBoundary } from '../../../lib/error/error-boundary';
 import { ErrorScreen } from '../../../lib/screens';
 
 describe('Error Boundary', () => {
+	let consoleErrorSpy: jest.SpyInstance;
+
+	beforeEach(() => {
+		consoleErrorSpy = jest.spyOn(console, 'error');
+		consoleErrorSpy.mockImplementation(jest.fn());
+	});
+
+	afterEach(() => {
+		consoleErrorSpy.mockRestore();
+	});
+
 	test('fallback component', () => {
 		const ThrowError = () => {
 			throw new Error('Test');
 		};
 
 		const { getByTestId } = render(
-			<ErrorBoundary fallback={<ErrorScreen />}>
+			<ErrorBoundary
+				fallback={<ErrorScreen />}
+				data-testid="error-boundary"
+			>
 				<ThrowError />
 			</ErrorBoundary>
 		);
