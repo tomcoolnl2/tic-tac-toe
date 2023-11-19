c6bd4f06c5857f5c136a958769a83f815d9a645b
coolt <tom.cool@tomtom.com>
2023-11-19 21:01:14 +0800

    ci: trigger build

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -3,6 +3,7 @@ import * as Rx from 'rxjs';
 import { AppStore } from '@tic-tac-toe/core';
 import * as TTTModel from '@tic-tac-toe/model';
 import * as TTTUI from '@tic-tac-toe/ui';
+
 import { isDevEnvironment } from '@tic-tac-toe/debug';
 
 export const App: React.FC = () => {
