77410bde4ec9abf9f9847faacd2892c9e357b4f4
coolt <tom.cool@tomtom.com>
2023-11-20 17:14:32 +0800

    test: updated input tests

--- ui/src/tests/snapshots/components/input.snapshot.test.tsx
+++ ui/src/tests/snapshots/components/input.snapshot.test.tsx
@@ -2,13 +2,20 @@ import { render } from '@testing-library/react';
 import { Input } from '../../../lib/components';
 
 test('Input component snapshot test', () => {
-	const { container } = render(<Input id="username" value={'test'} />);
+	const { container } = render(
+		<Input id="username" value={'test'} onChange={jest.fn()} />
+	);
 	expect(container).toMatchSnapshot();
 });
 
 test('Input component password snapshot test', () => {
 	const { container } = render(
-		<Input id="password" type="password" value={'pwd'} />
+		<Input
+			id="password"
+			type="password"
+			value={'pwd'}
+			onChange={jest.fn()}
+		/>
 	);
 	expect(container).toMatchSnapshot();
 });
