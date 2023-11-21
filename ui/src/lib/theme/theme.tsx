062095c28b31f93725550e2e689e9d16387df4d2
coolt <tom.cool@tomtom.com>
2023-11-21 14:05:28 +0800

    feat: set a different bg color kfor desktop version

--- ui/src/lib/theme/theme.tsx
+++ ui/src/lib/theme/theme.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
 import './styles.scss';
 
 export const Theme: React.FC<{ children: React.ReactNode }> = ({
 	children,
-}) => <main>{children}</main>;
+}) => {
+	const theme = React.useMemo(() => window.electron?.theme ?? 'web', []);
+
+	React.useEffect(() => {
+		document.body.classList.add(theme);
+	}, [theme]);
+
+	return <main className={theme}>{children}</main>;
+};
