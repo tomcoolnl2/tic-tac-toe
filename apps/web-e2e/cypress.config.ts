a2c468e4f051b7249b67af5de68babfa94d9069b
coolt <tom.cool@tomtom.com>
2023-12-06 07:33:05 +0100

    test: setup cupress incl ci

--- apps/web-e2e/cypress.config.ts
+++ apps/web-e2e/cypress.config.ts
@@ -1,6 +1,13 @@
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
-import { defineConfig } from 'cypress';
+import { nxE2EPreset } from "@nx/cypress/plugins/cypress-preset";
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-	e2e: nxE2EPreset(__filename, { cypressDir: 'src', bundler: 'vite' }),
+  e2e: nxE2EPreset(__filename, { cypressDir: "src", bundler: "vite" }),
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
 });
