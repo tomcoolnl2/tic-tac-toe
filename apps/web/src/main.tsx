a285341cfc405c41cd2c7541dadf1d7774e88151
tomcoolnl2 <tomcoolnl@gmail.com>
2023-12-10 15:46:59 +0100

    refactor: removed package-lock, import amplify config json

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,39 +1,21 @@
 import { StrictMode } from 'react';
-import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import {
-	AWS_USER_POOL_APP_CLIENT_ID,
-	AWS_USER_POOL_ID,
-	AWS_IDENTOTY_POOL_ID,
-} from '@tic-tac-toe/constants';
 import * as ReactDOM from 'react-dom/client';
 import * as TTTUI from '@tic-tac-toe/ui';
 import { App } from './app/app';
 
-// import awsExports from './aws-exports';
-// Amplify.configure(awsExports);
+import { Amplify } from 'aws-amplify';
+import amplifyConfig from './auth/amplifyconfiguration.json';
 
-Amplify.configure({
-	Auth: {
-		Cognito: {
-			identityPoolId: AWS_IDENTOTY_POOL_ID,
-			userPoolId: AWS_USER_POOL_ID,
-			userPoolClientId: AWS_USER_POOL_APP_CLIENT_ID,
-			// allowGuestAccess: false,
-		},
-	},
-});
+Amplify.configure(amplifyConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
 	<StrictMode>
-		<Authenticator.Provider>
-			<TTTUI.Context.AuthProvider>
-				<TTTUI.Context.ContentProvider>
-					<App />
-				</TTTUI.Context.ContentProvider>
-			</TTTUI.Context.AuthProvider>
-		</Authenticator.Provider>
+		<TTTUI.Context.AuthProvider>
+			<TTTUI.Context.ContentProvider>
+				<App />
+			</TTTUI.Context.ContentProvider>
+		</TTTUI.Context.AuthProvider>
 	</StrictMode>
 );
