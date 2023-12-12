d22998aa3712f6b6de3835b3a6d812391dc4238c
tomcoolnl2 <tomcoolnl@gmail.com>
2023-12-12 12:29:52 +0100

    chore: revert deletion of crucial app files

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import * as TTTUI from '@tic-tac-toe/ui';
+import { App } from './app/app';
+
+// import { Amplify } from 'aws-amplify';
+// import amplifyConfig from './auth/amplifyconfiguration.json';
+
+// Amplify.configure(amplifyConfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+	<StrictMode>
+		<TTTUI.Context.ContentProvider>
+			<App />
+		</TTTUI.Context.ContentProvider>
+	</StrictMode>
+);
