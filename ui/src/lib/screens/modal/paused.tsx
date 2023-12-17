a7763cd3627c588f00aa1a1c454e7a87c80d8338
tomcoolnl2 <tomcoolnl@gmail.com>
2023-12-17 01:47:05 +0100

    feat: add save game button to paused modal screen

--- ui/src/lib/screens/modal/paused.tsx
+++ ui/src/lib/screens/modal/paused.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import { useSaveGameManager } from '@tic-tac-toe/database';
 import type { AppScreenContent } from '../../context/content/model';
 import { Divider, Grid } from '../../core';
-import { Button } from '../../components';
+import { Button, Icon } from '../../components';
 import { useInterfaceHandlers } from '../../hooks';
 
 interface Props {
 	content: AppScreenContent;
 }
 
-export const PausedModalScreen: React.FC<Props> = ({ content }) => {
+export const PausedModalScreen: React.FC<Props> = React.memo(({ content }) => {
+	//
 	const { handleResumeGame } = useInterfaceHandlers();
 	const { addSaveGame } = useSaveGameManager();
+
 	return (
 		<>
 			<h1>{content.title}</h1>
 			<Divider margin="vertical-l" />
-			<Grid colGap="l">
-				<Button variant="primary" onClick={handleResumeGame}>
+			<Grid cols={2} colGap="l">
+				<Button variant="primary" onClick={addSaveGame}>
 					{content.cta2}
+					<Icon name="icon-save" />
 				</Button>
 				<Button variant="secondary" onClick={handleResumeGame}>
 					{content.cta1}
+					<Icon name="icon-play-sign" />
 				</Button>
 			</Grid>
 		</>
 	);
-};
+});
