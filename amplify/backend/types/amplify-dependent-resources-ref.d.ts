df7859c38e220e0d8a060576d8e52b878f1cddbd
tomcoolnl2 <tomcoolnl@gmail.com>
2023-12-17 14:15:23 +0100

    chore: removed conflicting AppSync Apis

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "tictactoe": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
